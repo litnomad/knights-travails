c8e7bbfed8088e340f13115fccc71ddb3b573937
litnomad <128262375+litnomad@users.noreply.github.com>
2026-07-20 15:17:38 -0400

    clean up

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ function knightMoves(start, end) {
   const queue = [[start, [start]]];
   const visited = new Set();
 
-  let result;
   while (queue.length !== 0) {
     const curr = queue.shift();
 
     if (curr[0][0] == end[0] && curr[0][1] == end[1]) {
-      result = curr[1];
-      return `${JSON.stringify(result)} in ${result.length} steps!`;
+      return `${JSON.stringify(curr[1])} in ${curr[1].length} steps!`;
     }
 
     const move = getMoves(curr[0], visited);
